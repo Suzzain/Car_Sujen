885e399f01a6a76cf9c266bbf74a1e0540f8c6f0
Suzzain <gurungsujen@gmail.com>
2019-04-12 21:19:13 +0545

    Initial commit

---
+++ app/src/main/java/com/e/car_sujen/CreateCar.java
@@ -0,0 +1,22 @@
+package com.e.car_sujen;
+
+public class CreateCar {
+    private String make, year, color, price, engine, Answer;
+    private int count;
+    public CreateCar(String make, String year, String color, String price, String engine, int count) {
+        this.make = make;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.engine = engine;
+        this.count = count;
+    }
+
+    public String CreatesCar(){
+
+
+        Answer= "The is Vehicle No. "+count+"\n"+" Manufacturer: "+make+" Current Value: "+price+ " Effective engine Size: "+engine+"\n";
+        return Answer;
+
+    }
+}
